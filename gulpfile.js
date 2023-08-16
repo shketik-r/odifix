cd14b411fc583c0f2c9861ad8702c9ef041db2eb
rusel28 <ruslans3101@gmail.com>
2023-08-16 20:01:18 +0300

    v2

--- gulpfile.js
+++ gulpfile.js
@@ -178,6 +178,14 @@ const build = series(
 );
 const dev = parallel(build, watchFiles, serve);
 
+
+const ghPages = require('gulp-gh-pages');
+
+gulp.task('deploy', function() {
+  return gulp.src('./dist/**/*')
+    .pipe(ghPages());
+});
+
 /* Exports Tasks */
 exports.html = html;
 exports.php = php; // Добавлено
