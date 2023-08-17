b12384098f3501fffe22ca03dffb365d4d248f73
Aleksandr <solalex.s.a@gmail.com>
2023-08-17 17:24:41 +0300

    v2

--- dist/assets/js/index.js
+++ dist/assets/js/index.js
@@ -1,4 +1,142 @@
 
 $(document).ready(function () {
-  $("#phone").inputmask({"mask": "+7 (99) 99-99-99"});
+  $("#phone").inputmask({ "mask": "+7 (99) 99-99-99" });
 });
+
+
+$(document).ready(function () {
+
+ 
+  $('#accordion > ul > li > a').click(function () {
+    var checkElement = $(this).next();
+
+    $('#accordion li').removeClass('active');
+    $(this).closest('li>ul').css({
+      display:'flex'
+  });
+
+
+    if ((checkElement.is('ul')) && (checkElement.is(':visible'))) {
+      $(this).closest('li').removeClass('active');
+      checkElement.slideUp('normal');
+    }
+
+    if ((checkElement.is('ul')) && (!checkElement.is(':visible'))) {
+      $('#accordion ul ul:visible').slideUp('normal');
+      checkElement.slideDown('normal');
+    }
+
+    if (checkElement.is('ul')) {
+      return false;
+    } else {
+      return true;
+    }
+  });
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const dbPrice = [
+//   {
+//     title: "Замена дисплея (оригинал)",
+//     description: "Дисплей разбился, нет изображения, не работает сенсорное стекло",
+//     oldPrice: 2600,
+//     newPrice: 2600,
+//   },
+//   {
+//     title: "Замена дисплея (оригинал)",
+//     description: "Дисплей разбился, нет изображения, не работает сенсорное стекло",
+//     oldPrice: 2600,
+//     newPrice: 2600,
+//   },
+//   {
+//     title: "Замена дисплея (оригинал)",
+//     description: "Дисплей разбился, нет изображения, не работает сенсорное стекло",
+//     oldPrice: 2600,
+//     newPrice: 2600,
+//   },
+//   {
+//     title: "Замена дисплея (оригинал)",
+//     description: "Дисплей разбился, нет изображения, не работает сенсорное стекло",
+//     oldPrice: 2600,
+//     newPrice: 2600,
+//   },
+//   {
+//     title: "Замена дисплея (оригинал)",
+//     description: "Дисплей разбился, нет изображения, не работает сенсорное стекло",
+//     oldPrice: 2600,
+//     newPrice: 2600,
+//   },
+//   {
+//     title: "Замена дисплея (оригинал)",
+//     description: "Дисплей разбился, нет изображения, не работает сенсорное стекло",
+//     oldPrice: 2600,
+//     newPrice: 2600,
+//   },
+
+// ]
+
+// function showPrices(db) {
+//   let priceList = document.getElementById('priceList');
+//   for (let i = 0; i <= db.length; i++) {
+//     let li = document.createElement("li");
+//     li.classList.add("content-services__list")
+//     li.innerHTML = `
+//     <div>
+//     <p class="content-services__title" >${db[i].title}</p>
+//       <p class="content-services__description" >${db[i].description}</p>
+//     </div>
+//     <div>
+//       <p class="content-services__old-price">${db[i].oldPrice} p</p>
+//       <p class="content-services__new-price">${db[i].newPrice} p</p>
+//     </div>
+// `
+//     priceList.append(li)
+//   }
+// }
